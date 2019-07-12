b2958013fdf07c8ecb92ee681beba1a2e46c0256
Pál Sára <pal.sara.ajandek@gmail.com>
2019-07-12 13:00:29 +0200

    remove function

--- scripts/projektSara.js
+++ scripts/projektSara.js
@@ -1,5 +1,6 @@
-window.addEventListener('load', Window_LoadHandler, false);
-function Window_LoadHandler() {
+window.addEventListener('load', windowLoadHandler, false);
+
+function windowLoadHandler() {
   User.init();
 }
 var User = {
@@ -34,20 +35,31 @@ var User = {
           usersTemplate += `<td><input type="text" class="input--disabled" disabled value="${users[i][memberName]}"></td>`;
         }
       }
-      usersTemplate += '<td><button>Szerkesztés</button> <button class="display--none">x</button><button class="display--none">✓</button></td>';
-      usersTemplate += '<td><button>Törlés</button></td>';
+      usersTemplate += `<td><button  dataid="${users[i].id}">Szerkesztés</button> <button class="display--none" dataid="${users[i].id}">x</button><button class="display--none" dataid="${users[i].id}">✓</button></td>`;
+      usersTemplate += `<td><button onclick="User.remove()" dataid="${users[i].id}">Törlés</button></td>`;
       usersTemplate += '</tr>';
     }
 
     document.querySelector('.users__data').innerHTML = usersTemplate;
   },
   remove() {
-
+    var nodeTD = event.target;
+    var nodeTDID = parseInt(nodeTD.getAttribute('dataid'), 10);
+    for (var i = 0; i < this.data.length; i++) {
+      if (nodeTDID === this.data[i].id) {
+        this.data.splice(i, 1);
+        break;
+      }
+    }
+    this.showAll(this.data);
   },
   create() {
 
   },
   store() {
+
+  },
+  edit() {
 
   }
 
